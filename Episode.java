edfbbada5d7574949becd68a7ea6b714ba8feaa6
JMGlass <mylesglass@hotmail.com>
2016-03-04 01:16:24 -0600

    Update Episode.java

--- Episode.java
+++ Episode.java
@@ -1,18 +1,35 @@
+public class Episode extends Media {
 
-public class Episode {
-
-	String title;
-	String year;
-	String season;
-	String number;
+	private String title;
+	private String year;
+	private String season;
+	private String number;
 	boolean suspendedStatus;
 	
-	public Episode() {
-		// TODO Auto-generated constructor stub
+	public Episode(String data) {
+				
+		// Splits the data into pieces and stores them in an array
+		String[] temp1 = data.split("\\("); //Split for year
+		String[] temp2 = data.split("\\{"); //Split for episode title
+		String[] temp3 = data.split("#"); // Split for episode number and season number
+		
+		if (data.contains("{")){
+
+			if (data.contains("#")){
+				this.season = temp3[1].substring(temp3[1].indexOf(".") - 1, temp3[1].indexOf("."));
+				this.number = temp3[1].substring(temp3[1].indexOf(".") + 1, temp3[1].indexOf(")"));
+			}
+			else if (temp2[1] != null)
+			{
+				this.title = temp2[1].substring(0, temp2[1].indexOf("}")); //THIS IS WHERE IM STUCKKKKKKK, index out of range when I substring
+			}
+		}
 	}
 	
 	public Episode(String title, String year, String season, String number, boolean suspendedStatus) {
-		
+		this.title = title;
+		this.year = year;
+
 	}
 	
 	public String getTitle() {
