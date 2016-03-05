222df4f14fb9f30ff0c7bd7f9cdd122757285bab
dancressman <dancressman2@gmail.com>
2016-03-05 00:15:25 -0600

    Possible Final Update

--- Episode.java
+++ Episode.java
@@ -1,18 +1,46 @@
+public class Episode extends Media implements Comparable<Episode> {
 
-public class Episode {
-
-	String title;
-	String year;
-	String season;
-	String number;
+	private String title;
+	private String year;
+	private String season;
+	private String number;
 	boolean suspendedStatus;
 	
-	public Episode() {
-		// TODO Auto-generated constructor stub
+	public Episode(String data) {
+				
+		// Splits the data into pieces and stores them in an array
+		//String[] temp1 = data.split("\\("); //Split for year
+		String[] temp2 = data.split("\\{"); //Split for episode title
+		String[] temp3 = data.split("#"); // Split for episode number and season number
+		
+		if (data.contains("{")){
+
+			if (data.contains("#")){
+				this.season = temp3[1].substring(temp3[1].indexOf(".") - 1, temp3[1].indexOf("."));
+				this.number = temp3[1].substring(temp3[1].indexOf(".") + 1, temp3[1].indexOf(")"));
+				this.title = temp2[1].substring(0, temp2[1].indexOf("("));
+			}
+			else if (temp2.length == 3){
+				this.title = temp2[2].substring(0, temp2[2].indexOf("}"));
+				}
+				else {
+					this.title = temp2[1].substring(0, temp2[1].indexOf("}"));
+				}
+			if(data.contains("SUSPENDED"))
+				suspendedStatus = true;
+			else
+				suspendedStatus = false;
+			
+			this.year = data.substring(data.length() - 4, data.length());
+		}
 	}
 	
 	public Episode(String title, String year, String season, String number, boolean suspendedStatus) {
-		
+		this.title = title;
+		this.year = year;
+		this.season = season;
+		this.number = number;
+		this.suspendedStatus = suspendedStatus;
 	}
 	
 	public String getTitle() {
@@ -36,26 +64,24 @@ public class Episode {
 	}
 	
 	public void setTitle(String title) {
-		
+		this.title = title;
 	}
-	
-	/** 
-	 * This method is used to sort Episode objects based on their title. 
-	 * <P> 
-	 * Algorithm<br> 
-	 * 1: Searches through each string for the first differing character<br>
-	 * 2: Compares the two characters to see with comes first alphabetically or returns 0 if they are the same string<br>
-	 * 3: Returns 1 or -1 based on which String comes first alphabetically<br> 
-	 * 
-	 * @param             otherEpisode      A series object to be compared to the Episode object called with compareTo                      
-	 * @return            int         The method returns an integer after comparing the two 
-	 * 								  objects and determining which title comes first alphabetically.
-	 *  
-	 * <dt><b>Conditions:</b> 
-	 * <dd>PRE  -         The two objects compared are unsorted in their ArrayList 
-	 * <dd>POST -         After running through the method, the Collections class will use 
-	 * 					  the returned value to sort the two objects in their ArrayList
-	 */
+
+	public String toString() {
+		// Begin episodeString
+		String episodeString = "EPISODE: ";
+
+		// Add title to string
+		episodeString += this.title;
+
+		// Add year to string
+		if (this.year.contains("?"))
+			episodeString += " (UNSPECIFIED)";
+		else
+			episodeString += " (" + this.year + ")";
+
+		return episodeString;
+	}
 	
 	@Override
 	public int compareTo(Episode otherEpisode) {
@@ -80,5 +106,4 @@ public class Episode {
 			return 1;
 		}
 	}
-
 }
