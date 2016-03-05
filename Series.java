7a29c403da3dc78fc956369c78705355f2a83d91
JMGlass <mylesglass@hotmail.com>
2016-03-04 22:56:52 -0600

    Series good

--- Series.java
+++ Series.java
@@ -33,6 +33,8 @@ public class Series extends Media {
 			
 			this.title = pieces[0].substring(1, pieces[0].length() - 2);
 			this.beginYear = pieces[1].substring(pieces[1].length() - 9, pieces[1].length()-5);
+			
+			if (pieces[1].contains("????"))
 			this.endYear = pieces[1].substring(pieces[1].length() - 4, pieces[1].length());
 		}
 	}
@@ -85,12 +87,50 @@ public class Series extends Media {
 		
 	}
 	
-	public int compareTo(Series otherSeries) {
-		return 0;
-	}
 	
 	public String toString(){
 		return null;
+	}
+	
+	/** 
+	 * This method is used to sort Series objects based on their title. 
+	 * <P> 
+	 * Algorithm<br> 
+	 * 1: Searches through each string for the first differing character<br>
+	 * 2: Compares the two characters to see with comes first alphabetically or returns 0 if they are the same string<br>
+	 * 3: Returns 1 or -1 based on which String comes first alphabetically<br>
+	 * 
+	 * @param             otherSeries      A series object to be compared to the series object called with compareTo                      
+	 * @return            int         The method returns an integer after comparing the two 
+	 * 								  objects and determining which title comes first alphabetically.
+	 *  
+	 * <dt><b>Conditions:</b> 
+	 * <dd>PRE  -         The two objects compared are unsorted in their ArrayList 
+	 * <dd>POST -         After running through the method, the Collections class will use 
+	 * 					  the returned value to sort the two objects in their ArrayList
+	 */
+	
+	@Override
+	public int compareTo(Series otherSeries) {
+		int i = 0;
+		
+		/*checks for the the first character in a String that differs between two titles*/
+		while(otherSeries.getTitle().charAt(i) == this.title.charAt(i) && i < (this.title.length() - 1)){
+			++i;
+		}
+		
+		/*returns 0 if it is the same string*/
+		if (i == this.title.length() - 1){
+			return 0;
+		}
+		/*returns -1 if otherSeries comes after the compared series alphabetically*/
+		else if (otherSeries.getTitle().charAt(i) > this.title.charAt(i)){
+			return -1;
+		}
+		/*returns 1 if otherSeries comes before the compared series alphabetically*/
+		else{
+			return 1;
+		}
 	}
 
 }
