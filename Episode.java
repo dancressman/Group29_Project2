5bddb80f338a7319a19c6ca75acd2358d679d7a8
JMGlass <mylesglass@hotmail.com>
2016-03-04 20:50:46 -0600

    Episode Complete

--- Episode.java
+++ Episode.java
@@ -1,3 +1,4 @@
+
 public class Episode extends Media {
 
 	private String title;
@@ -9,21 +10,27 @@ public class Episode extends Media {
 	public Episode(String data) {
 				
 		// Splits the data into pieces and stores them in an array
-		String[] temp1 = data.split("\\("); //Split for year
+		//String[] temp1 = data.split("\\("); //Split for year
 		String[] temp2 = data.split("\\{"); //Split for episode title
 		String[] temp3 = data.split("#"); // Split for episode number and season number
 		
-		//If current line is an episode, it will contain a "{"
 		if (data.contains("{")){
-			
+
 			if (data.contains("#")){
 				this.season = temp3[1].substring(temp3[1].indexOf(".") - 1, temp3[1].indexOf("."));
 				this.number = temp3[1].substring(temp3[1].indexOf(".") + 1, temp3[1].indexOf(")"));
+				this.title = temp2[1].substring(0, temp2[1].indexOf("("));
 			}
-			else if (temp2[1] != null)
-			{
-				this.title = temp2[1].substring(0, temp2[1].indexOf("}")); //THIS IS WHERE IM STUCKKKKKKK, index out of range when I substring
-			}
+			else if (temp2.length == 3){
+				this.title = temp2[2].substring(0, temp2[2].indexOf("}"));
+				}
+				else{
+				this.title = temp2[1].substring(0, temp2[1].indexOf("}"));
+				}
+			if(data.contains("SUSPENDED"))
+				suspendedStatus = true;
+			else
+				suspendedStatus = false;
 		}
 	}
 	
