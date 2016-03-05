765319afff5e2dd5ec2d4941d6115fcaee71e285
dancressman <dancressman2@gmail.com>
2016-03-05 00:16:11 -0600

    Update Series.java

--- Series.java
+++ Series.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
 
-public class Series extends Media {
+public class Series extends Media implements Comparable<Series> {
 
-	/** Stores the title of the movie. */
+	/** Stores the title of the series. */
 	private String title;
-	/** Stores the release year of the movie. */
-	private String year;
+	/** Stores the release year of the series. */
+	private String beginYear;
+	/** Stores the release year of the series. */
+	private String endYear;
 	/**
 	 * Stores the episodes
 	 */
 	private ArrayList<Episode> episodes;
-
+	
 	/*
 	 * INCOMPLETE
 	 * 
@@ -18,62 +20,81 @@ public class Series extends Media {
 	 */
 	
 	public Series(String data) {
+		// Creates an array of strings to hold the pieces
+		String[] pieces = new String[5];
 		
+		// Splits the data into pieces and stores them in an array
+		String[] temp = data.split("\\(");
+		
+		//Checks to see if the line doesnt contain an episode, if it doesnt then its a series title
+		if (!data.contains("{")){
+			pieces[0] = temp[0];
+			pieces[1] = temp[1];
+			
+			this.title = pieces[0].substring(1, pieces[0].length() - 2);
+			this.beginYear = pieces[1].substring(pieces[1].length() - 9, pieces[1].length()-5);
+			this.endYear = pieces[1].substring(pieces[1].length() - 4, pieces[1].length());
+		
+		}
+		this.episodes = new ArrayList<Episode>();
 	}
 	
-	public Series(String title, String year, ArrayList<Episode> episodes) {
-		
+	public Series(String title, String beginYear, String endYear, ArrayList<Episode> episodes) {
+		this.title = title;
+		this.beginYear = beginYear;
+		this.endYear = endYear;
+		this.episodes = episodes;
 	}
 	
+	public Series copyOf() {
+		ArrayList<Episode> blankEpisodes = new ArrayList<Episode>();
+
+		Series copy = new Series(title, beginYear, endYear, blankEpisodes);
+
+		return copy;
+
+	}
 	public String getTitle() {
-		return null;
+		return title;
 	}
 	
-	public String getYear() {
-		return null;
+	public String getBeginYear() {
+		return beginYear;
+	}
+	
+	public String getEndYear() {
+		return endYear;
 	}
 	
 	public ArrayList<Episode> getEpisodes() {
-		return null;
+		return episodes;
 	}
 	
 	public void setTitle(String title) {
-		
+		this.title = title;
 	}
 	
-	public void setYear(String year) {
-		
+	public void setBeginYear(String beginYear) {
+		this.beginYear = beginYear;
+	}
+	
+	public void setEndYear(String endYear) {
+		this.endYear = endYear;
 	}
 	
 	public void setEpisodes(ArrayList<Episode> episodes) {
-		
+		this.episodes = episodes;
 	}
 	
 	public void addEpisode(Episode newEpisode) {
-		
+		this.episodes.add(newEpisode);
 	}
 	
 	public void addEpisode(String title, String year, String season, String number, boolean suspendedStatus) {
+		Episode newEpisode = new Episode(title, year, season, number, suspendedStatus);
 		
+		this.episodes.add(newEpisode);
 	}
-	
-	/** 
-	 * This method is used to sort Series objects based on their title. 
-	 * <P> 
-	 * Algorithm<br> 
-	 * 1: Searches through each string for the first differing character<br>
-	 * 2: Compares the two characters to see with comes first alphabetically or returns 0 if they are the same string<br>
-	 * 3: Returns 1 or -1 based on which String comes first alphabetically<br>
-	 * 
-	 * @param             otherSeries      A series object to be compared to the series object called with compareTo                      
-	 * @return            int         The method returns an integer after comparing the two 
-	 * 								  objects and determining which title comes first alphabetically.
-	 *  
-	 * <dt><b>Conditions:</b> 
-	 * <dd>PRE  -         The two objects compared are unsorted in their ArrayList 
-	 * <dd>POST -         After running through the method, the Collections class will use 
-	 * 					  the returned value to sort the two objects in their ArrayList
-	 */
 	
 	@Override
 	public int compareTo(Series otherSeries) {
@@ -99,7 +120,22 @@ public class Series extends Media {
 	}
 	
 	public String toString(){
-		return null;
+		// Begin seriesString
+		String seriesString = "SERIES: ";
+
+		// Add title to string
+		seriesString += this.title;
+
+		// Add begin year to string
+		seriesString += " (" + this.beginYear;
+
+		// Add end year to string
+		if (this.endYear.contains("?"))
+			seriesString += "UNSPECIFIED";
+		else
+			seriesString += this.endYear + ")";
+
+		return seriesString;
 	}
 
 }
