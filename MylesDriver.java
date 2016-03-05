698aaabe537dee58d1ca89fb561c5afc71abd278
JMGlass <mylesglass@hotmail.com>
2016-03-04 20:54:49 -0600

    MylesDriver Tester
    filereader method, displayArrayList (toString) method, Initial ArrayLists, program arguments code, all in here.

--- MylesDriver.java
+++ MylesDriver.java
@@ -22,7 +22,7 @@ public class Driver {
 
 		readFile(file); //Read the file line by line and stores them into the arraylists declared above
 		
-		displayArrayList(episodeList); //A method I use for testing only, change the arraylist to test different ones (seriesList, movieList)
+		displayArrayList(seriesList); //A method I use for testing only, change the arraylist to test different ones (seriesList, movieList)
 	
 	}
 	
@@ -40,20 +40,25 @@ public class Driver {
 			
 			
 			/********* For Movies ********/  //uncommoent the code to use it
+			//Media mediaObj = new Media(nextline);  
+			//mediaList.add(mediaObj);
+			
+			
+			/********* For Movies ********/  //uncommoent the code to use it
 			//Movie movieObj = new Movie(nextline);  
 			//movieList.add(movieObj);
 			
 			
 			/********* For Series ********/   //uncommoent the code to use it
-			//Series seriesObj = new Series(nextline);
-			//if(!nextline.contains("{"))
-				//seriesList.add(seriesObj);
+			Series seriesObj = new Series(nextline);
+			if(!nextline.contains("{"))
+				seriesList.add(seriesObj);
 			
 			
 			/********* For Episodes ********/    //uncommoent the code to use it
-			Episode episodeObj = new Episode(nextline);
-			if(nextline.contains("{"))
-				episodeList.add(episodeObj);
+			//Episode episodeObj = new Episode(nextline);
+			//if(nextline.contains("{"))
+				//episodeList.add(episodeObj);
 			
 			
 			nextline = br.readLine();
@@ -63,14 +68,19 @@ public class Driver {
 	}
 	
 	//Uncommoent the code to use it and change the arraylist type to episode, series, or movie, depending on what you are testing
-	public static void displayArrayList(ArrayList<Episode> listToBeDisplayed) {
-		for (int i = 0; i < 30;i++){ //listToBeDisplayed.size()
-			System.out.println(listToBeDisplayed.get(i).getTitle());  // For Series, episodes, or movies
+	public static void displayArrayList(ArrayList<Series> listToBeDisplayed) {
+		for (int i = 0; i < listToBeDisplayed.size();i++){ //listToBeDisplayed.size()
+			//System.out.println(listToBeDisplayed.get(i).getTitle());  // For Series, episodes, or movies
+			
+			System.out.println(listToBeDisplayed.get(i).getTitle() + " ");
+			//System.out.print(listToBeDisplayed.get(i).getSeason() + " ");
+			//System.out.print(listToBeDisplayed.get(i).getSuspendedStatus() + " ");
+			//System.out.println(listToBeDisplayed.get(i).getNumber());
 			//System.out.println(listToBeDisplayed.get(i).getSeason());  //For episodes
 			//System.out.println(listToBeDisplayed.get(i).getNumber());  //For episodes
 			//System.out.println(listToBeDisplayed.get(i).getBegYear());  //For movie or episodes
-			//System.out.println(listToBeDisplayed.get(i).getBeginYear());  //For Series
-			//System.out.println(listToBeDisplayed.get(i).getEndYear());  //For Series
+			System.out.println(listToBeDisplayed.get(i).getBeginYear());  //For Series
+			System.out.println(listToBeDisplayed.get(i).getEndYear());  //For Series
 			//System.out.println(listToBeDisplayed.get(i).getReleaseMethod());  //For Movies
 		}
 	}
